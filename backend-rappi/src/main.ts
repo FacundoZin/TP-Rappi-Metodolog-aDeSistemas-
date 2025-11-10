2f286cc0bc572a109e02d4c59e06a75e7e20b2b8
facu <facundozin10@gmail.com>
2025-11-09 23:10:29 -0300

    implementamos swagger

--- backend-rappi/src/main.ts
+++ backend-rappi/src/main.ts
@@ -1,15 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(
     new ValidationPipe({
-      whitelist: true, // ignora propiedades no definidas en el DTO
-      forbidNonWhitelisted: true, // lanza error si llegan propiedades extra
-      transform: true, // convierte el payload en una instancia del DTO
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
     }),
   );
 
@@ -23,7 +24,6 @@ async function bootstrap() {
 
   const document = SwaggerModule.createDocument(app, config);
 
-  // 2. Montar la interfaz de Swagger en una ruta (ej: /api)
   SwaggerModule.setup('api', app, document);
   await app.listen(process.env.PORT ?? 3000);
 }
