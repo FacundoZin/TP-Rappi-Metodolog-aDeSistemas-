f88f9c4daf91fcde250cef2028d1dbb986522f2c
facu <facundozin10@gmail.com>
2025-11-14 13:34:59 -0300

    _

--- FrontendRappi-Prototype/src/App.vue
+++ FrontendRappi-Prototype/src/App.vue
@@ -1,15 +1,14 @@
 <template>
   <div class="min-h-screen bg-gray-50 text-gray-800">
-
-    <NavBar @search="searchParams = $event" />
+    <NavBar v-if="$route.name !== 'login'" @search="searchParams = $event" />
 
     <router-view :searchParams="searchParams"></router-view>
   </div>
 </template>
 
 <script setup>
-import { ref } from 'vue';
-import NavBar from './components/NavBar.vue';
+import { ref } from "vue";
+import NavBar from "./components/NavBar.vue";
 
-const searchParams = ref({ text: '', type: 'name' })
+const searchParams = ref({ text: "", type: "name" });
 </script>
